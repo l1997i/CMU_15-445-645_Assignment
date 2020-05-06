381f133337cd117a9f7b8c1992f7ac523c860ceb
l1997i <lil147@pitt.edu>
2020-05-06 15:58:44 +0800

    feat: query 8 complete

---
+++ hw1/placeholder/q8_number_of_actors.sql
@@ -0,0 +1,24 @@
+/*
+ * List the number of actors / actresses who have appeared in any
+ * title with Mark Hamill (born in 1951).
+ *  
+ * Details: Print only the total number of actors and actresses. 
+ * The answer should include Mark Hamill himself.
+ */ 
+
+WITH
+    MH(person_id) AS (
+        SELECT DISTINCT(crew.title_id)
+        FROM people
+        JOIN crew
+        ON
+            crew.person_id == people.person_id
+            AND people.name == 'Mark Hamill'
+            AND people.born == 1951
+    )
+
+SELECT COUNT(DISTINCT(crew.person_id)) AS count
+FROM crew
+WHERE
+    (category == 'actor' OR category == 'actress')
+    AND crew.title_id IN MH;
